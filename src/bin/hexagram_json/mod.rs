ed63a17069f9b7228e5784710285b8048d634f93
Kerry Snyder <krry@me.com>
2019-04-14 13:46:07 -0500

    match changing line with index using shim

--- src/bin/hexagram_json/mod.rs
+++ src/bin/hexagram_json/mod.rs
@@ -115,9 +115,11 @@ impl HexagramInfo for HexagramJsonInfo {
     }
 
     fn get_line_meanings(&self, changing_lines: &[usize]) -> Vec<&ChangingLineMeaning> {
+        let shim: usize = 1;
         self._lines
             .iter()
-            .filter(|&line_meaning| changing_lines.contains(&line_meaning.position))
+            .filter(|&line_meaning|
+                    changing_lines.contains(&(line_meaning.position - shim)))
             .collect()
     }
 
