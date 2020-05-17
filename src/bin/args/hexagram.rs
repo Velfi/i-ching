d372985e9f77eabfd50194fe41af77d0fcd429f7
oro13 <pcgentle@gmail.com>
2020-05-16 17:10:23 -0700

    fix typo

--- src/bin/args/hexagram.rs
+++ src/bin/args/hexagram.rs
@@ -6,7 +6,7 @@ pub const HELP: &str =
     "(A number 1-64){n}Look up a hexagram by number in the King Wen sequence.{n} ";
 pub const LONG_HELP: &str = "\
 Look up a hexagram by number in the King Wen sequence.{n}
-example:{n}iching -h 42{n} ";
+example:{n}iching -x 42{n} ";
 pub const LONG: &str = NAME;
 pub const NAME: &str = "hexagram";
 pub const SHORT: &str = "x";
